4bdcdfa2f272562bbf9a8e32e27d9e837c0526d6
Eric-Nobrega <eric.nobrega@city.ac.uk>
2022-04-17 20:18:19 +0100

    added light mode & fixed bugs

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const colorBtn = document.getElementById("colorChangeBtn");
+const navbar = document.getElementById("navbar");
+const linkOne = document.getElementById("linkOne");
+const linkTwo = document.getElementById("linkTwo");
+const contactSection = document.getElementById("contactsection");
+const footer = document.getElementsByClassName("footer");
+
 colorBtn.addEventListener("click", function () {
   var element = document.body;
   element.classList.toggle("dark-mode");
+
+  navbar.classList.remove("navbar-dark");
+  navbar.classList.remove("bg-dark");
+  navbar.classList.add("navbar-light");
+  navbar.classList.add("bg-light");
+  navbar.classList.add("light-nav");
+
+  linkOne.classList.add("text-black");
+  linkTwo.classList.add("text-black");
+
+  contactSection.classList.remove("contactsection");
+  contactSection.classList.add("bg-secondary");
+
+  footer.classList.add("bg-white");
+  footer.classList.add("text-black");
 });
